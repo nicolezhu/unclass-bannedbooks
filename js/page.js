a3067cc2873d6fca9bccfa871903a4baa454eebe
nicolezhu <nzhu94@gmail.com>
2014-05-18 21:40:16 -0500

    basic layout of book pages and chapters

---
+++ js/page.js
@@ -0,0 +1,140 @@
+var Page = (function() {
+
+	var $container = $( '#container' ),
+		$bookBlock = $( '#bb-bookblock' ),
+		$items = $bookBlock.children(),
+		itemsCount = $items.length,
+		current = 0,
+		bb = $( '#bb-bookblock' ).bookblock( {
+			speed : 700,
+			perspective : 2000,
+			shadowSides	: 0.8,
+			shadowFlip	: 0.4,
+			onEndFlip : function(old, page, isLimit) {
+				
+				current = page;
+				// updateNavigation
+				updateNavigation( isLimit );
+				// initialize jScrollPane on the content div for the new item
+				setJSP( 'init' );
+				// destroy jScrollPane on the content div for the old item
+				setJSP( 'destroy', old );
+
+			}
+		} ),
+		$navNext = $( '#bb-nav-next' ),
+		$navPrev = $( '#bb-nav-prev' ).hide(),
+		$menuItems = $container.find( 'ul.chapters > li' ),
+		transEndEventNames = {
+			'WebkitTransition': 'webkitTransitionEnd',
+			'MozTransition': 'transitionend',
+			'OTransition': 'oTransitionEnd',
+			'msTransition': 'MSTransitionEnd',
+			'transition': 'transitionend'
+		},
+		transEndEventName = transEndEventNames[Modernizr.prefixed('transition')],
+		supportTransitions = Modernizr.csstransitions;
+
+	function init() {
+
+		// initialize jScrollPane on the content div of the first item
+		setJSP( 'init' );
+		initEvents();
+	}
+	
+	function initEvents() {
+
+		// add navigation events
+		$navNext.on( 'click', function() {
+			bb.next();
+			return false;
+		} );
+
+		$navPrev.on( 'click', function() {
+			bb.prev();
+			return false;
+		});
+		
+		// add swipe events
+		$items.on( {
+			'swipeleft'	: function( event ) {
+				if( $container.data( 'opened' ) ) {
+					return false;
+				}
+				bb.next();
+				return false;
+			},
+			'swiperight': function( event ) {
+				if( $container.data( 'opened' ) ) {
+					return false;
+				}
+				bb.prev();
+				return false;
+			}
+		});
+
+		// click a menu item
+		$menuItems.on('click', function() {
+
+			var $el = $(this),
+				idx = $el.index(),
+				jump = function() {
+					bb.jump( idx + 1 );
+				};
+
+			if (current != idx) {
+				console.log("going to page");
+				jump();
+			} else {
+				console.log("already on the page");
+			}
+
+			return false;
+			
+		});
+
+		// reinit jScrollPane on window resize
+		$( window ).on( 'debouncedresize', function() {
+			// reinitialise jScrollPane on the content div
+			setJSP( 'reinit' );
+		});
+	}
+
+	function setJSP( action, idx ) {
+		
+		var idx = idx === undefined ? current : idx,
+			$content = $items.eq( idx ).children( 'div.content' ),
+			apiJSP = $content.data( 'jsp' );
+		
+		if( action === 'init' && apiJSP === undefined ) {
+			$content.jScrollPane({verticalGutter : 0, hideFocus : true });
+		}
+		else if( action === 'reinit' && apiJSP !== undefined ) {
+			apiJSP.reinitialise();
+		}
+		else if( action === 'destroy' && apiJSP !== undefined ) {
+			apiJSP.destroy();
+		}
+
+	}
+
+	function updateNavigation( isLastPage ) {
+		
+		if( current === 0 ) {
+			$navNext.show();
+			$navPrev.hide();
+		}
+		else if( isLastPage ) {
+			$navNext.hide();
+			$navPrev.show();
+		}
+		else {
+			$navNext.show();
+			$navPrev.show();
+		}
+
+	}
+
+	return { init : init };
+
+})();
